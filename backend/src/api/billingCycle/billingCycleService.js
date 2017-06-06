9ab5a0adbb666c71216c1eeabb41f4e5955ed318
Lucas Helal <lucasghelal@gmail.com>
2017-06-06 13:29:19 -0300

    Mapeando Rotas

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,1 +1,7 @@
 const BillingCycle = require('./billingCycle');
+
+BillingCycle.methods(['get', 'post', 'put', 'delete']);
+/* Mantem os esquemas para o put */
+BillingCycle.updateOptions({new: true, runValidators: true});
+
+module.exports = BillingCycle;
